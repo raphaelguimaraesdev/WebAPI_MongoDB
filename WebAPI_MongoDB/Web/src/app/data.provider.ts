d170f090241bf67af02d8d30a9889e96d486a3dd
Raphael Guimarães <raphaelguimaraes.dev@hotmail.com>
2018-09-15 09:41:15 -0300

    Criação data provider.

---
+++ WebAPI_MongoDB/Web/src/app/data.provider.ts
@@ -0,0 +1,8 @@
+export class Provider {
+
+  private _url = 'http://localhost:55202/api/values/'
+
+   public static GetUrlPostCliente() : string {
+     return 'http://localhost:55202/api/values/post';
+   }
+}
--- WebAPI_MongoDB/Web/src/app/pessoa.service.ts
+++ WebAPI_MongoDB/Web/src/app/pessoa.service.ts
@@ -1,31 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {Pessoa} from "./Shared/pessoa.model"
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
-
+import {Pessoa} from "./Shared/pessoa.model";
+import {Provider} from "./data.provider";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PessoaService {
 
+  private _headers: HttpHeaders;
 
-  private _url = 'http://localhost:55202/api/values/post'
+
 
 
   constructor(private _http: HttpClient) {
+    this._headers =  new HttpHeaders({ 'Content-Type': 'application/json' });
   }
 
   post(pessoa: Pessoa) {
     const body = JSON.stringify(pessoa);
-    return this._http.post(this._url, body, httpOptions).subscribe(
+    return this._http.post(Provider.GetUrlPostCliente(), body, this._headers).subscribe(
       response => console.log(response),
       err => console.log(err)
     );
   }
 }
 
-}
 
